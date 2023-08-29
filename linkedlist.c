287024120033554f7896b1f0951c9f51d9102b34
Sahil Dudhal <129080375+SAHILDUDHAL21@users.noreply.github.com>
2023-08-29 22:38:13 +0530

    Add files via upload

---
+++ linkedlist.c
@@ -0,0 +1,58 @@
+#include<stdio.h> 
+ #include<stdlib.h> 
+ #include<malloc.h> 
+  
+ struct node 
+ { 
+        int data; 
+        struct node *next; 
+ }; 
+  
+ struct node* create(struct node *head) 
+ { 
+       int n,i; 
+       struct node *newnode,*temp; 
+  
+       printf("\n Enter Limit :: "); 
+       scanf("%d",&n); 
+  
+       for(i=-0;i<n;i++) 
+       { 
+               newnode=(struct node *)malloc(sizeof(struct node)); 
+               printf("Enter Value :: "); 
+               scanf("%d",&newnode->data); 
+  
+               newnode->next=NULL; 
+  
+               if(head==NULL) 
+               { 
+                          head=newnode; 
+                          temp=newnode;               
+               } 
+               else 
+               { 
+                           temp->next=newnode; 
+                           temp=newnode; 
+               } 
+       } 
+       return head; 
+ } 
+  
+ void display(struct node *head) 
+ { 
+         struct node *temp; 
+  
+         for(temp=head;temp!=NULL;temp=temp->next) 
+         { 
+                 printf("%d\t",temp->data); 
+         } 
+ } 
+  
+ int main() 
+ { 
+         struct node *head=NULL; 
+  
+         head=create(head); 
+  
+         display(head); 
+ }
